76b383bb18fb05cb02b1fdac197d90c879d16282
FreshLeaf8865 <cmd.bsolution@outlook.com>
2018-05-07 14:22:54 -0700

    adding events to tooltip, realtimeshowing

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function sortByKey(array, key) {
 }
 
 var getRooms = function(data) {
-	let rooms = []
+	let tempRooms = [], rooms = []
 	
 	let opnumber = 0
 	let eventCode = ''
@@ -39,7 +39,7 @@ var getRooms = function(data) {
 		if (opnumber !== data[i].OPNumber) {
 			opnumber = data[i].OPNumber
 	
-			rooms[opnumber] = {
+			tempRooms[opnumber] = {
 				INPR: [], // 'INPR'
 				INAN: [], // 'INAN'
 				INTH: [],	// 'INTH'
@@ -56,30 +56,42 @@ var getRooms = function(data) {
 		}
 
 		if (eventCode !== '') {
-			rooms[opnumber][eventCode].push(data[i])
+			tempRooms[opnumber][eventCode].push(data[i])
+		}
+	}
+
+	for (let i in tempRooms) {
+		rooms[i] = []
+
+		for (let j in roomSep) {
+			if (tempRooms[i][roomSep[j]].length != 0) {
+				rooms[i].push({roomCode: roomSep[j], events: tempRooms[i][roomSep[j]]})
+			}
 		}
 	}
 	
 	return rooms
 }
 
-var w = 1900;
-var h = 800;
+var margin = {
+	top : 40,
+	right : 40,
+	bottom : 20,
+	left : 40
+}
 
 d3.csv("/csv/operations.csv", function(operations) {
 	d3.csv("/csv/events.csv", function(events) {
+		var w = document.body.clientWidth - margin.left - margin.right
+		var h = operations.length * 24 + 180
+
 		var svg = d3.selectAll(".svg")
-							.append("svg")
-							.attr("width", w)
-							.attr("height", h);
+								.append("svg")
+								.attr("width", w)
+								.attr("height", h)
 
 		// operations = sortByKey(operations, 'USAGE');
 		var today = new Date();
-
-		for (let i = 0; i < operations.length; i++) {
-			operations[i].STARTTIME = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + operations[i].STARTTIME);
-			operations[i].ENDTIME = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + operations[i].ENDTIME);
-		}
 
 		var timeDomainStart = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " 07:48:00");
 		var timeDomainEnd = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " 21:48:00");
@@ -146,57 +158,64 @@ d3.csv("/csv/operations.csv", function(operations) {
 											 	})
 											 	.attr("opacity", 0.2);
 
-			var rectRooms = svg.append('g');
+			var rectRooms = svg.append('g').selectAll("rect")
 
 			for (let i in operations) {
 				if (typeof rooms[operations[i].OPNUMBER] !== 'undefined') {
-					var keys = Object.keys(rooms[operations[i].OPNUMBER])
+					var yPosition = i * theGap + theTopPad
 
-					for (let j in keys) {
-						var room = rooms[operations[i].OPNUMBER][keys[j]]
+					var rectRoom = rectRooms.data(rooms[operations[i].OPNUMBER]).enter()
+																	.append("rect")
+																	.attr("rx", 3)
+																	.attr("ry", 3)
+																	.attr("x", function(d) {
+																		var barStartTime = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + d.events[0].EVENTTIME)
+																		return x(barStartTime) + theSidePad
+																	})
+																	.attr("y", yPosition)
+																	.attr("width", function(d) {
+																		var barStartTime = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + d.events[0].EVENTTIME)
+																		var barEndTime = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + d.events[d.events.length - 1].EVENTTIME)
+																		return x(barEndTime) - x(barStartTime)
+																	})
+																	.attr("height", theBarHeight)
+																	.attr("stroke", "none")
+																	.attr("class", function(d) {
+																		return d.roomCode
+																	})
 
-						if (room.length != 0) {
-							barStartTime = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + room[0].EVENTTIME);
-							barEndTime = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + room[room.length - 1].EVENTTIME);
+					var output = document.getElementById("tooltip")
 
-							var xPosition = x(barStartTime) + theSidePad
-							var yPosition = i * theGap + theTopPad
-							var barWidth  = x(barEndTime) - x(barStartTime)
+					rectRoom.on('mouseover', function(d) {
+						var	tooltip = "<p>OPNumber: " + operations[i].OPNUMBER + '</p><p style="margin-top: 10px;">Events</p><br>'
 
-							var rectRoom = rectRooms.append("rect")
-																			.attr("rx", 3)
-																			.attr("ry", 3)
-																			.attr("x", xPosition)
-																			.attr("y", yPosition)
-																			.attr("width", barWidth)
-																			.attr("height", theBarHeight)
-																			.attr("stroke", "none")
-																			.attr("class", keys[j])
-
-							var output = document.getElementById("tooltip")
-
-							rectRoom.on('mouseover', function(e) {
-								var	tooltip = "<p>OPNumber: " + operations[i].OPNUMBER + '</p><p style="margin-top: 10px;">Events</p>'
-
-								for (let k in room) {
-									tooltip = tooltip + '<p>' + a[k].EVENTCODE + ': ' + a[k].EVENTTIME + '</p>'
-								}
-
-								output.innerHTML = tooltip;
-								output.style.display = "block";
-							})
-							.on('mousemove', function(d) {
-								output.style.top = (d3.event.layerY + 10) + 'px'
-								output.style.left = (d3.event.layerX - 25) + 'px'
-							})
-							.on('mouseout', function() {
-								output.style.display = "none";
-							});
+						for (let i in d.events) {
+							tooltip += '<p>' + d.events[i].EVENTCODE + ': ' + d.events[i].EVENTTIME + '</p>'
 						}
-					}
+
+						output.innerHTML = tooltip;
+						output.style.display = "block";
+					})
+					.on('mousemove', function(d) {
+						output.style.top = (d3.event.layerY + 10) + 'px'
+						output.style.left = (d3.event.layerX - 25) + 'px'
+					})
+					.on('mouseout', function() {
+						output.style.display = "none";
+					});
 				}
 			}
 
+			svg.append('g').append("line")
+										.attr("x1", function() {
+											return x(today) + theSidePad
+										})
+										.attr("x2", function() {
+											return x(today) + theSidePad
+										})
+										.attr("y1", 45)
+										.attr("y2", h - 50)
+										.attr("style", "stroke:rgb(255,0,0);stroke-width:1")
 
 			// var rectangles = svg.append('g')
 			// 										.selectAll("rect")
@@ -361,3 +380,205 @@ d3.csv("/csv/operations.csv", function(operations) {
 		}
 	});
 });
+
+timer = setInterval(function() {
+	d3.csv("/csv/operations.csv", function(operations) {
+		d3.csv("/csv/events.csv", function(events) {
+			var w = document.body.clientWidth - margin.left - margin.right
+			var h = operations.length * 24 + 180
+
+			d3.select("svg").remove()
+
+			var svg = d3.selectAll(".svg")
+									.append("svg")
+									.attr("width", w)
+									.attr("height", h)
+
+			var today = new Date();
+
+			var timeDomainStart = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " 07:48:00");
+			var timeDomainEnd = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " 21:48:00");
+			var x = d3.time.scale().domain([timeDomainStart, timeDomainEnd]).range([0, w]).clamp(true);
+			var usages = [];
+
+			for (let i = 0; i < operations.length; i++) {
+				usages.push(operations[i].USAGE);
+			}
+
+			var usagesUnfiltered = usages;
+
+			usages = checkUnique(usages);
+
+			let rooms = getRooms(events)
+
+			makeGant(operations, rooms, w, h);
+
+			var title = svg.append("text")
+										.text("Real-Time Theatre Dashboard")
+										.attr("x", w / 2)
+										.attr("y", 25)
+										.attr("text-anchor", "middle")
+										.attr("font-size", 18)
+										.attr("fill", "#009FFC");
+
+			function makeGant(operations, rooms, pageWidth, pageHeight) {
+				var barHeight = 20;
+				var gap = barHeight + 4;
+				var topPadding = 75;
+				var sidePadding = 65;
+
+				var colorScale = d3.scale.linear()
+													.domain([0, usages.length])
+													.range(["#00B9FA", "#F95002"])
+													.interpolate(d3.interpolateHcl);
+
+				makeGrid(sidePadding, topPadding, pageWidth, pageHeight);
+				drawRects(operations, rooms, gap, topPadding, sidePadding, barHeight, colorScale, pageWidth, pageHeight);
+				vertLabels(gap, topPadding, sidePadding, barHeight, colorScale);
+			}
+
+			function drawRects(operations, rooms, theGap, theTopPad, theSidePad, theBarHeight, theColorScale, w, h) {
+				var bigRects = svg.append("g")
+													.selectAll("rect")
+												 	.data(operations)
+												 	.enter()
+												 	.append("rect")
+												 	.attr("x", 0)
+												 	.attr("y", function(d, i) {
+														return i * theGap + theTopPad - 2;
+													})
+												 	.attr("width", function(d) {
+														return w;
+												 	})
+												 	.attr("height", theGap)
+												 	.attr("stroke", "none")
+												 	.attr("fill", function(d){
+														for (let i = 0; i < usages.length; i++) {
+															if (d.USAGE == usages[i]) {
+																return d3.rgb(theColorScale(i % 2));
+															}
+														}
+												 	})
+												 	.attr("opacity", 0.2);
+
+				var rectRooms = svg.append('g').selectAll("rect")
+
+				for (let i in operations) {
+					if (typeof rooms[operations[i].OPNUMBER] !== 'undefined') {
+						var yPosition = i * theGap + theTopPad
+
+						var rectRoom = rectRooms.data(rooms[operations[i].OPNUMBER]).enter()
+																		.append("rect")
+																		.attr("rx", 3)
+																		.attr("ry", 3)
+																		.attr("x", function(d) {
+																			var barStartTime = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + d.events[0].EVENTTIME)
+																			return x(barStartTime) + theSidePad
+																		})
+																		.attr("y", yPosition)
+																		.attr("width", function(d) {
+																			var barStartTime = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + d.events[0].EVENTTIME)
+																			var barEndTime = new Date(today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + " " + d.events[d.events.length - 1].EVENTTIME)
+																			return x(barEndTime) - x(barStartTime)
+																		})
+																		.attr("height", theBarHeight)
+																		.attr("stroke", "none")
+																		.attr("class", function(d) {
+																			return d.roomCode
+																		})
+
+						var output = document.getElementById("tooltip")
+
+						rectRoom.on('mouseover', function(d) {
+							var	tooltip = "<p>OPNumber: " + operations[i].OPNUMBER + '</p><p style="margin-top: 10px;">Events</p><br>'
+
+							for (let i in d.events) {
+								tooltip += '<p>' + d.events[i].EVENTCODE + ': ' + d.events[i].EVENTTIME + '</p>'
+							}
+
+							output.innerHTML = tooltip;
+							output.style.display = "block";
+						})
+						.on('mousemove', function(d) {
+							output.style.top = (d3.event.layerY + 10) + 'px'
+							output.style.left = (d3.event.layerX - 25) + 'px'
+						})
+						.on('mouseout', function() {
+							output.style.display = "none";
+						});
+					}
+				}
+
+				svg.append('g').append("line")
+											.attr("x1", function() {
+												return x(today) + theSidePad
+											})
+											.attr("x2", function() {
+												return x(today) + theSidePad
+											})
+											.attr("y1", 45)
+											.attr("y2", h - 50)
+											.attr("style", "stroke:rgb(255,0,0);stroke-width:1")
+			}
+
+			function makeGrid(theSidePad, theTopPad, w, h) {
+				var xAxis = d3.svg.axis()
+											.scale(x)
+											.orient('bottom')
+											.tickSize(-h + theTopPad + 20, 0, 0)
+											.tickFormat(d3.time.format('%H:%M'));
+
+				var grid = svg.append('g')
+											.attr('class', 'grid')
+											.attr('transform', 'translate(' + theSidePad + ', ' + (h - 50) + ')')
+											.call(xAxis)
+											.selectAll("text")
+											.style("text-anchor", "middle")
+											.attr("fill", "#000")
+											.attr("stroke", "none")
+											.attr("font-size", 10)
+											.attr("dy", "1em");
+			}
+
+			function vertLabels(theGap, theTopPad, theSidePad, theBarHeight, theColorScale) {
+				var numOccurances = [];
+				var prevGap = 0;
+
+				for (let i = 0; i < usages.length; i++) {
+					numOccurances[i] = [usages[i], getCount(usages[i], usagesUnfiltered)];
+				}
+
+				var axisText = svg.append("g")
+													.selectAll("text")
+													.data(numOccurances)
+													.enter()
+													.append("text")
+													.text(function(d) {
+														return d[0];
+													})
+													.attr("x", 10)
+													.attr("y", function(d, i) {
+														if (i > 0) {
+															for (let j = 0; j < i; j++) {
+																prevGap += numOccurances[i - 1][1];
+																return d[1] * theGap / 2 + prevGap * theGap + theTopPad;
+															}
+														} else {
+															return d[1] * theGap / 2 + theTopPad;
+														}
+													})
+													.attr("font-size", 11)
+													.attr("text-anchor", "start")
+													.attr("text-height", 14)
+													.attr("fill", function(d) {
+														for (let i = 0; i < usages.length; i++) {
+															if (d[0] == usages[i]) {
+																return d3.rgb(theColorScale(i)).darker();
+															}
+														}
+													});
+
+			}
+		});
+	});
+}, 30000)
